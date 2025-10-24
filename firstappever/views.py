63afa8dac09cf0d5d9f682cb0bbb9711288a55b1
ERF404 <ERF404@users.noreply.github.com>
2025-10-24 14:18:08 +0000

    imported template
    imported template and fixed js, img and css of its home page

--- firstappever/views.py
+++ firstappever/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render (request, 'firstappever/first_template.html')
+    return render (request, 'firstappever/index.html')
 
 def nigga(request):
     return render (request, 'firstappever/nigga.html')
