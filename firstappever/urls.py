9798c6f55282adaf0633198009acd23e73b8a17a
ERF404 <ERF404@users.noreply.github.com>
2025-10-30 15:25:34 +0000

    made blog app
    Made blog app and fixed the two pages of it but the direction from blog pages to index, about and contact doesn't work.

--- firstappever/urls.py
+++ firstappever/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from firstappever.views import homepage, nigga, about, contact
 
+app_name = 'firstappever'
+
 urlpatterns = [
-    path('', homepage),
-    path('nigga/', nigga),
-    path('contact/', contact),
-    path('about/', about)
+    path('', homepage, name='index'),
+    path('nigga/', nigga, name='nigga'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about')
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firstappever.urls')),
-    path('about/', include('firstappever.urls')),
-    path('contact/', include('firstappever.urls'))
+    path('blog/', include('blog.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
