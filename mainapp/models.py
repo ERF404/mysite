29c420c0798b809cd810b64ffee6699d16d69a41
ERF404 <ERF404@users.noreply.github.com>
2025-11-13 15:20:17 +0000

    Registered Contact Models
    - Registered Contact Models
    - Customized the admin panel of Blog Post and mainapp Contact
    - Added meta class (ordering) for both models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
@@ -8,3 +9,8 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+    def __str__(self):
+        return self.name
