0dbe0810db8a409e14f8f9bd3be6aa3c7efdc3fb
ERF404 <ERF404@users.noreply.github.com>
2025-11-17 19:14:44 +0000

    pid
    - Replaced all {% url 'blog:single' %} with # in blog templates
    - Showing posts based on id
    - Linked posts buttons to their respected post details

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 def blog_view (request):
+
     posts = Post.objects.filter(
         status=1,
         published_date__lte=timezone.now()
         )
+    
     context = {'posts': posts}
     return render (request, 'blog/blog-home.html', context)
 
-def blog_single (request):
-    return render (request, 'blog/blog-single.html')
+def blog_single (request,pid):
+
+    post = get_object_or_404(Post,id=pid)
+    context = {'post': post}
+
+    return render (request, 'blog/blog-single.html', context)
