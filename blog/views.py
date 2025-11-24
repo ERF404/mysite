f89d8e7398cb5f763b1f4eb088eaec642cf318ec
ERF404 <ERF404@users.noreply.github.com>
2025-11-24 12:38:45 +0000

    Limited access to not published posts in blog
    - Changed the code in blog view to limit the access to unpublished posts

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,13 @@ def blog_view (request):
     return render (request, 'blog/blog-home.html', context)
 
 def blog_single (request,pid):
-    post = get_object_or_404(Post,id=pid)
+
+    posts = Post.objects.filter(
+        status=1,
+        published_date__lte=timezone.now()
+        )
+    
+    post = get_object_or_404(posts,id=pid)
 
     post.counted_view += 1
     post.save(update_fields=['counted_view'])
