d7b7a893308f21932aef32fe7d81319c3da6b732
ERF404 <ERF404@users.noreply.github.com>
2025-11-25 14:45:01 +0000

    Author and image section
    - Added author and image section to blog models and customized blog-home template accordingly
    - Installed Pillow

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title', 'counted_view', 'status', 'created_date', 'published_date')
-    list_filter = ('status',)
+    list_display = ('title', 'counted_view','author', 'status', 'created_date', 'published_date')
+    list_filter = ('status','author')
     search_fields = ['title', 'content']    
 
 admin.site.register(Post,PostAdmin)
