06f15418de78f0411f426a7dca628b0adeac6e1a
ERF404 <ERF404@users.noreply.github.com>
2025-12-02 17:35:50 +0000

    Search bar
    - Fixed the search bar

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
+from django.db.models import Q
 
 
 def blog_view (request,**kwargs):
@@ -42,3 +43,21 @@ def blog_single (request,pid):
     }
 
     return render (request, 'blog/blog-single.html', context)
+
+def blog_search(request):
+
+    posts = Post.objects.filter(
+        status=1,
+        published_date__lte=timezone.now()
+        )
+    
+    if request.method == 'GET':
+        if s:= request.GET.get('s'):
+            posts = posts.filter(
+                Q(content__contains=s) |
+                Q(title__contains=s)
+            )
+
+    context = {'posts': posts}
+    return render (request, 'blog/blog-home.html', context)
+    
